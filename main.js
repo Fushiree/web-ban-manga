22d7147ceb47e46d6f52309ee427c47b4bd822ff
Fushiree Trần <quannro2004@gmail.com>
2024-11-04 01:19:18 +0700

    add product

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
-const itemsliderbar = document.querySelectorAll(".cartegory-left-li"); // Thêm dấu chấm ở đây
+// danh sách tùy chọn(index)
+const itemsliderbar = document.querySelectorAll(".cartegory-left-li");
 itemsliderbar.forEach(function(menu, index) {
     menu.addEventListener("click", function() {
         menu.classList.toggle("block");
     });
 });
+//end danh sách tùy chọn(index)
 
+// product
+const bigimg = document.querySelector(".product-content-left-big-img img")
+const smallimg = document.querySelectorAll(".product-content-left-small-img img")
+smallimg.forEach(function(imgItem,x){
+    imgItem.addEventListener("click", function(){
+        bigimg.src = imgItem.src;
+    });
+});
 
+const mota = document.querySelector(".mota")
+const chitiet = document.querySelector(".chitiet")
+
+if (mota) {
+    mota.addEventListener("click", function() {
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none";
+        document.querySelector(".product-content-right-bottom-content-mota").style.display = "block";
+    });
+}
+
+if (chitiet) {
+    chitiet.addEventListener("click", function() {
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block";
+        document.querySelector(".product-content-right-bottom-content-mota").style.display = "none";
+    });
+}
+
+const butTon = document.querySelector(".product-content-right-bottom-top");
+if (butTon) {
+    butTon.addEventListener("click", function() {
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB");
+    });
+}
+
+//end product
